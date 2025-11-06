1402b4db85514bc649755d16d750ea56339d26b9
Musa Ahmad Abdullahi <musaahotoro34@gmail.com>
2025-11-06 20:29:12 +0300

    Create readFile method to read file and store in a 2D array

--- src/main/java/com/musaahotoro/Main.java
+++ src/main/java/com/musaahotoro/Main.java
@@ -1,7 +1,31 @@
 package com.musaahotoro;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        readFile();
+    }
 
+    public static void readFile() {
+        String filepath = "maze.txt";
+        File file = new File(filepath);
+        int size = 15;
+        int[][] maze = new int[size][size];
+
+        try(Scanner reader = new Scanner(file)) {
+            for(int i = 0; i < size; i++) {
+                String line = reader.nextLine();
+                for(int j = 0; j < size; j++) {
+                    maze[i][j] = Character.getNumericValue(line.charAt(j));
+                    System.out.print(maze[i][j]);
+                }
+                System.out.println();
+            }
+        }catch(FileNotFoundException e) {
+                System.out.println("File" + filepath + " not found");
+        }
     }
 }
