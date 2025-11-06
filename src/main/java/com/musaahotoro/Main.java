83a94e4b57fb203470c7144382acbd0577b7fe16
Musa Ahmad Abdullahi <musaahotoro34@gmail.com>
2025-11-06 20:45:54 +0300

    let the Main be just the driver

--- src/main/java/com/musaahotoro/Main.java
+++ src/main/java/com/musaahotoro/Main.java
@@ -6,26 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        readFile();
+
     }
 
-    public static void readFile() {
-        String filepath = "maze.txt";
-        File file = new File(filepath);
-        int size = 15;
-        int[][] maze = new int[size][size];
-
-        try(Scanner reader = new Scanner(file)) {
-            for(int i = 0; i < size; i++) {
-                String line = reader.nextLine();
-                for(int j = 0; j < size; j++) {
-                    maze[i][j] = Character.getNumericValue(line.charAt(j));
-                    System.out.print(maze[i][j]);
-                }
-                System.out.println();
-            }
-        }catch(FileNotFoundException e) {
-                System.out.println("File" + filepath + " not found");
-        }
-    }
 }
