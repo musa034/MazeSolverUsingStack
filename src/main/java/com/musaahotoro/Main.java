8bb6626b0cb153eb831721ea60ce118b476c8ca4
Musa Ahmad Abdullahi <musaahotoro34@gmail.com>
2025-11-09 17:42:08 +0300

    fix: remove printing of solve() method, since it is now void

--- src/main/java/com/musaahotoro/Main.java
+++ src/main/java/com/musaahotoro/Main.java
@@ -2,14 +2,12 @@ package com.musaahotoro;
 
 public class Main {
     public static void main(String[] args) {
-
         int size = 15;
 
         MazeReader reader = new MazeReader("maze.txt");
         int[][] maze = reader.getMaze();
 
         MazeSolver solver = new MazeSolver(maze, size);
-        System.out.println(solver.solve());
-        solver.printPath();
+        solver.solve();
     }
 }
