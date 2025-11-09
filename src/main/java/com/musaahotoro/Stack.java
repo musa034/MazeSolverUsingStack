d97d10c616c01242e0737572ec3f7bb6a2973d0f
Musa Ahmad Abdullahi <musaahotoro34@gmail.com>
2025-11-09 09:12:50 +0300

    feat: add Stack class to manage Coordinate objects with push, pop, top and utility such as isEmpty, getTopIndex, getElementAt methods

--- src/main/java/com/musaahotoro/Stack.java
+++ src/main/java/com/musaahotoro/Stack.java
@@ -1,5 +1,56 @@
 package com.musaahotoro;
 
 public class Stack {
+    private int topIndex;
+    final private int size;
+    final private Coordinate[] items;
+
+    Stack(int size) {
+        this.size = size;
+        items = new Coordinate[size];
+        topIndex = -1;
+    }
+
+    public void push(Coordinate item) {
+        if(topIndex != size - 1) {
+            topIndex++;
+            items[topIndex] = item;
+        }
+    }
+
+    public Coordinate pop() {
+        if(topIndex == -1) {
+            System.err.println("Stack is Empty");
+        }
+        Coordinate c = items[topIndex];
+        topIndex--;
+
+        return c;
+    }
+
+    public Coordinate top() {
+        if(topIndex == -1) {
+            System.out.println("Stack is Empty");
+        }
+        return items[topIndex];
+    }
+
+    public boolean isEmpty() {
+        if(topIndex == -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public int getTopIndex() {
+        return topIndex;
+    }
+
+    public Coordinate getElementAt(int index) {
+        return (Coordinate) items[index];
+    }
+
+
+
 
 }
