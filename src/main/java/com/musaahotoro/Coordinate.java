ee1afce04d6af595c1a7316f175230e12dd7fbc2
Musa Ahmad Abdullahi <musaahotoro34@gmail.com>
2025-11-09 09:02:44 +0300

    feat: add Coordinate class to represent maze position, with column & row getters, toString and equals methods

---
+++ src/main/java/com/musaahotoro/Coordinate.java
@@ -0,0 +1,35 @@
+package com.musaahotoro;
+
+public class Coordinate {
+    private int rows;
+    private int columns;
+
+    Coordinate(int rows, int columns) {
+        if(rows >= 0 && columns >= 0) {
+            this.rows = rows;
+            this.columns = columns;
+        }
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public String toString() {
+        return "(" + rows + ", " + columns + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Coordinate other = (Coordinate) obj;
+        return this.rows == other.rows && this.columns == other.columns;
+    }
+
+
+}
